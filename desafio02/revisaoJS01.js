20b254f655c403c6c4127b5d5a09c6696e47756a
JosyRamos <josileiaramos97@gmail.com>
2022-02-25 17:33:48 -0400

    revisoes em javascript

---
+++ desafio02/revisaoJS01.js
@@ -0,0 +1,31 @@
+
+
+        var nome = "josi!"
+        const idade = 21.5;
+      //  console.log(`${nome}, tem ${idade} anos`);
+        var nome02="josileia";
+        let sobreNome ="ramos rodrigues"
+
+      //  console.log(nome02 === nome);
+
+   // console.log(nome == idade);
+   // console.log(nome == idade ? "sim é igual": "não é igual");
+   // console.log(Math.floor(idade));
+/*var array2= [  nome,
+    nome02,
+    sobreNome,
+    idade]
+   
+
+
+//console.table(array2)*/
+
+function valores(texto01){
+    return texto01
+}
+ console.log(valores("Josileia ramos"),valor(21));
+
+function valor(varlo){
+    return varlo
+
+}
---
+++ desafio02/revisaojs02.js
@@ -0,0 +1,23 @@
+//fundamentos com array
+
+//desafios atividades: lista com 2 dimenssoes;
+
+/*const alunos = ['joao', 'juliana', 'caio','ana']
+const mediasAlunos = [10,7,9,6];
+const mediaTotal = [5,4,3,2,1]
+
+const alunosJuntos = alunos.concat([mediasAlunos]);
+
+let listaNotasAlunos = [alunos , mediasAlunos]
+
+console.log(`${listaNotasAlunos[0][2]}, a sua media é ${listaNotasAlunos[1][0]}`);
+console.table(alunosJuntos)*/
+
+//function aluno(nome){
+  //  console.log('josi') 
+//}
+//aluno();
+const numeros = [1,3,200,300,400,600];
+/*for(let j=0; j<numeros.length;j++){
+    console.table(numeros[j]);
+}*/
